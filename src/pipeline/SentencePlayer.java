9bae797293b532ee1c3a91710887b9c7e2b98d39
Aaron <aaronrmm@gmail.com>
2018-11-25 16:19:08 -0500

    Added brooding emotion

--- src/pipeline/SentencePlayer.java
+++ src/pipeline/SentencePlayer.java
@@ -63,6 +63,7 @@ public class SentencePlayer extends javax.swing.JFrame  {
 		Quiescent(1.2, new Color(171, 210, 216)),//blue
 		Active(.5, new Color(200,200,150)),//orange
 		Calm(1.3, new Color(200,255,150)),//green
+		Brooding(1.3, new Color(165, 70, 58)),//dark red
 		Fear(0.6, new Color(194, 179, 214));//purple
 		
 	   double multiplier;
